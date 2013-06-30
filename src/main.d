4bf6c92694e567e401bb1bd7d98e663549668e07
xentec <artificial.i7@gmail.com>
2013-06-30 02:06:55 +0200

    Now with a fancy triangle...
    and some VBO, VAO, vertex and fragment shader and of course some linking in between

--- src/main.d
+++ src/main.d
@@ -1,4 +1,4 @@
-module ttgl;
+module ttgl.app;
 
 import std.stdio;
 import std.conv : to;
@@ -50,8 +50,89 @@ int main(string[] args) {
 	DerelictGL3.reload();
 	//##################################
 	//##################################
+
+	// Our triangle
+	float vertices[] = [
+		0.0f,	0.5f,	// Vertex 1 (X, Y)
+		0.5f,	-0.5f,	// Vertex 2 (X, Y)
+		-0.5f,	-0.5f,	// Vertex 3 (X, Y)
+	];
 	
-	// Greatness shall arise here... 
+	const char* vertexSource = 
+		`	#version 150
+
+			in vec2 position;
+
+			void main()	{
+				gl_Position = vec4(position, 0.0, 1.0); //Put vertices in right position
+			}
+		`;
+	
+	const char* fragmentSource = 
+		`	#version 150
+
+			out vec4 outColor;
+
+			void main() {
+				outColor = vec4(1.0, 1.0, 1.0, 1.0); // Just paint them white
+			}
+		`;
+
+	// Create our Vertex Array Object
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	scope(exit) glDeleteVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	// Create vertex buffes to hold our vertices
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	scope(exit) glDeleteBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	// Upload the vertices
+	glBufferData(GL_ARRAY_BUFFER, float.sizeof*vertices.length, vertices.ptr, GL_STATIC_DRAW);
+
+	writeln("Compiling shaders...");
+
+	write("\tVertex... ");
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	scope(exit) glDeleteShader(vertexShader);
+	glShaderSource(vertexShader, 1, &vertexSource, null);
+	
+	glCompileShader(vertexShader);
+	if(!isShaderCompiled(vertexShader)) {
+		writeln("E: " ~ getShaderCompileLog(vertexShader));
+		throw new Exception("<");
+	} else
+		writeln("DONE");
+	
+	write("\tFragment... ");
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	scope(exit) glDeleteShader(fragmentShader);
+	glShaderSource(fragmentShader, 1, &fragmentSource, null);
+	
+	glCompileShader(fragmentShader);
+	if(!isShaderCompiled(fragmentShader)) {
+		writeln("E: " ~ getShaderCompileLog(fragmentShader));
+		throw new Exception("<");
+	} else
+		writeln("DONE");
+
+	writeln("Starting the shader program...");
+	GLuint shaderProgram = glCreateProgram();
+	scope(exit) glDeleteProgram(shaderProgram);
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	// Setting color vector output in fragment shader to outColor
+	glBindFragDataLocation(shaderProgram, 0, "outColor");
+	glLinkProgram(shaderProgram);
+	glUseProgram(shaderProgram);
+
+	// Tell our vertex shader how to use the vertices from our VBO
+	GLuint posAttrib = glGetAttribLocation(shaderProgram, "position");
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, null);
+	glEnableVertexAttribArray(posAttrib);
 	
 	//##################################
 	//##################################
@@ -59,7 +140,10 @@ int main(string[] args) {
 	while(!glfwWindowShouldClose(window)) {
 		//##############
 		
-		//...in some future.
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		//##############
 		// Spit it out
@@ -71,4 +155,21 @@ int main(string[] args) {
 	writeln("Exiting...");
 	// Don't forget the scopes! ^
 	return 0;
+}
+
+bool isShaderCompiled(in GLuint shader) {
+	GLint status;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+	return status ? true : false;
+}
+
+string getShaderCompileLog(in GLuint shader) {
+	GLint length;
+	char[] buffer;
+
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
+	buffer.length = length;
+	glGetShaderInfoLog(shader, length, null, buffer.ptr);
+
+	return std.string.chop(buffer).idup;
 }
