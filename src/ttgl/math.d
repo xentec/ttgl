8531e85d85a3a0c214a75a44d9c8cd48451e6687
xentec <artificial.i7@gmail.com>
2013-08-23 12:38:55 +0200

    Fixed compile: rad() got somehow lost

--- src/ttgl/math.d
+++ src/ttgl/math.d
@@ -5,6 +5,12 @@ import std.stdio;
 
 import gl3n.linalg;
 
+enum TAU = PI*2;
+
+real rad(in real angle) {
+	return angle*TAU/360.0;
+}
+
 @safe pure T linear_interpolate(T)(float t, in T[] points ...) {
 	if(points.length == 1)
 		return points[0];
