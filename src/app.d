7bf61d99bd7e174d57b5aa9cb1d25c836b8b528a
xentec <artificial.i7@gmail.com>
2013-11-21 16:39:24 +0100

    Fixed framebuffer repeating

--- src/app.d
+++ src/app.d
@@ -401,6 +401,8 @@ int main(string[] args) {
 
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 
 		glFramebufferTexture2D(GL_FRAMEBUFFER, 
 							   GL_COLOR_ATTACHMENT0, 
